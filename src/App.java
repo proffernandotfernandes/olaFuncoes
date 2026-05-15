6d65c0147ec04ad5bf1e0088f5bc6b256f9fe29f
FERNANDO TIMOTEO FERNANDES <fernando.tfernandes@senacsp.edu.br>
2026-05-15 20:25:38 -0300

    Versão inicial

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        float comissao = Utils.calcularPercentual(100.00f, 20.00f);
+        System.out.println(comissao);
+    }
+}
---
+++ src/Utils.java
@@ -0,0 +1,9 @@
+public class Utils {
+
+    public static float calcularPercentual(float valor, float percentual){
+
+        float percentualCalculado = (percentual/100);
+        return valor * percentualCalculado;
+    }
+
+}

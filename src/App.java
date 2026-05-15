c76edf0af2db625b7bdd1f5b23a84cc7816436d3
FERNANDO TIMOTEO FERNANDES <fernando.tfernandes@senacsp.edu.br>
2026-05-15 20:48:31 -0300

    Incluida função de calculo de acrescimo

--- src/App.java
+++ src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         float comissao = Utils.calcularPercentual(100.00f, 20.00f);
         System.out.println(comissao);
+
+        float novoValor = Utils.calcularAcrescimo(100.00f, 10.00f);
+        System.out.println(novoValor);
+
     }
 }
